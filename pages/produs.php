2cb79b8af220419377e47c6b192109303224af11
RSGDesign <radusebastiangabriel2001@gmail.com>
2026-02-04 23:58:37 +0200

    Remove unused font preloads from header and disable error reporting in produs.php

--- pages/produs.php
+++ pages/produs.php
@@ -4,10 +4,6 @@
  * Afișare informații produs, galerie imagini, opțiuni achiziție
  */
 
-// Debug - activează error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions_downloads.php';
